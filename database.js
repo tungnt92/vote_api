d880fe1276b57901f706b5dc2d5dab8aa95cc971
nhan1080 <luuhoangnhan1080@gmail.com>
2020-07-18 03:29:25 +0000

    form server

--- database.js
+++ database.js
@@ -27,6 +27,7 @@ let db = new sqlite3.Database(DBSOURCE, (err) => {
     //       // Table already created
     //     }
     //   });
+
     console.log('Connected to the SQLite database.');
     db.run(`CREATE TABLE user (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
